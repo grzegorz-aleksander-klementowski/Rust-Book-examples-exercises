dde71cfe574bc35bcc26620643be6a12b83e5f37
Grzegorz Aleksander Klementowski <125459323+grzegorz-aleksander-klementowski@users.noreply.github.com>
2024-02-02 19:33:47 +0100

    Additional chapter 5

---
+++ chapter_5/examples/ex4_struct_ip_address/target/debug/deps/ex3_struct_ip_address-078de83519ad086d.d
@@ -0,0 +1,5 @@
+/home/klemensgak/Projekty/src/Rust/Rust Book - The Rust Programming Language/chapter_5/examples/ex4_struct_ip_address/target/debug/deps/ex3_struct_ip_address-078de83519ad086d.rmeta: src/main.rs
+
+/home/klemensgak/Projekty/src/Rust/Rust Book - The Rust Programming Language/chapter_5/examples/ex4_struct_ip_address/target/debug/deps/ex3_struct_ip_address-078de83519ad086d.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_5/examples/ex4_struct_ip_address/target/debug/deps/ex3_struct_ip_address-e9d2019f75cb775c.d
@@ -0,0 +1,5 @@
+/home/klemensgak/Projekty/src/Rust/Rust Book - The Rust Programming Language/chapter_5/examples/ex4_struct_ip_address/target/debug/deps/ex3_struct_ip_address-e9d2019f75cb775c.rmeta: src/main.rs
+
+/home/klemensgak/Projekty/src/Rust/Rust Book - The Rust Programming Language/chapter_5/examples/ex4_struct_ip_address/target/debug/deps/ex3_struct_ip_address-e9d2019f75cb775c.d: src/main.rs
+
+src/main.rs:
