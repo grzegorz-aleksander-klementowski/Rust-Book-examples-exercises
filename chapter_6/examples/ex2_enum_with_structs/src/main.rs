82cd766b599a6da57a5a5af7135a2bf47e4324a7
Grzegorz Aleksander Klementowski <125459323+grzegorz-aleksander-klementowski@users.noreply.github.com>
2024-02-04 23:58:08 +0100

    Add files via upload

---
+++ chapter_6/examples/ex2_enum_with_structs/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    struct Ipv4Addr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+struct Ipv6Addr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum IpAddr {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
+
+}
+
