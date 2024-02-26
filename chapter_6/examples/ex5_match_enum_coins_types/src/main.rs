09db2860843018ace6e9e3429659f837317dddb5
Grzegorz Aleksander Klementowski <125459323+grzegorz-aleksander-klementowski@users.noreply.github.com>
2024-02-26 11:26:45 +0100

    Add files via upload

---
+++ chapter_6/examples/ex5_match_enum_coins_types/src/main.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+enum UsStates {
+        Alaska,
+        Alabama,
+        California, Texsas,
+        // -- snip --
+    }
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsStates),
+}
+
+fn value_in_cent(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn main() {
+    value_in_cent(Coin::Penny);
+    value_in_cent(Coin::Quarter(UsStates::Alaska));
+}
