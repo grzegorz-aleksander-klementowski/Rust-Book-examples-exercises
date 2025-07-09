fb5b9cec51c8fbae059cfc00fe021555489c4f3b
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-07-09 23:18:24 +0200

    Updating CI: change example in `chapter_4/examples/ex5_refferemces/src/main.rs` to avoid error during `cargo build`

--- chapter_4/examples/ex5_refferemces/src/main.rs
+++ chapter_4/examples/ex5_refferemces/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let reference_to_nothing = dangle();
+    let owned_string = no_dangle();
+    println!("Zwrócona wartość: {}", owned_string);
 }
 
-fn dangle() -> &String {
+// despite it's an example from the Rust Book – we return String instead &String to avoid errors
+fn no_dangle() -> String {
     let s = String::from("hello");
-
-    &s
+    s
 }
+
