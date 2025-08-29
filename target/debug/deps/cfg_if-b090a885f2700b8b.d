66c1b0b4f7087c4aa946d9cc58eda00f11a33848
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-08-29 08:32:12 +0200

    style: rustfmt run

---
+++ target/debug/deps/cfg_if-b090a885f2700b8b.d
@@ -0,0 +1,5 @@
+/home/klemens_gak/Pisma/prace/złomek/Rust-Book-examples-exercises/target/debug/deps/cfg_if-b090a885f2700b8b.d: /home/klemens_gak/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
+
+/home/klemens_gak/Pisma/prace/złomek/Rust-Book-examples-exercises/target/debug/deps/libcfg_if-b090a885f2700b8b.rmeta: /home/klemens_gak/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
+
+/home/klemens_gak/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs:
