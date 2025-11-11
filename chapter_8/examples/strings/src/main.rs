9da86233f1cb32afd8f795249c2e62dae193bc7b
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-11-11 11:27:03 +0100

    feat/chapter_8/examples/strings: Added to the example: Concatanation using `+` operator (works for many Strings), Better wersion of Concatanations of many Strings – `format!()` macro, Iterating over String (the correct way – indexing doesn't wokr), Printing bytes of a String.

--- chapter_8/examples/strings/src/main.rs
+++ chapter_8/examples/strings/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 #[allow(unused_variables)]
 fn main() {
     // Similarity with vectors
@@ -20,9 +22,29 @@ fn main() {
     s.push('d');
     println!("{s}");
 
-    // Concatanation using `+` operator
+    // Concatanation using `+` operator (works for many Strings)
     let s1 = String::from("Perun i ");
     // let s1_ref = &s1; `fn add(self, s: &str) -> String`–requires String, not &String
     let s2 = String::from("Rod");
     let s3 = s1 + &s2; // ← &s2 as a reference `&String` (it coerces the &String argument into a &str)
+
+    // Better wersion of Concatanations of many Strings – `format!()` macro.
+    let s1 = String::from("Perun");
+    let s2 = String::from("Rod");
+    let s3 = String::from("Wales");
+    let s = format!("{s1}, {s2} oraz {s3}"); // format! macro uses references so that this call doesn’t take ownership of any of its parameters.
+
+    // Iterating over String (the correct way – indexing doesn't wokr)
+    for c in "नमस्ते".chars() {
+        println!("{c}");
+    }
+
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+
+    // Printing bytes of a String
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
 }
