0680a5fcbdc088f2adc6cb10f5ae7c3a90831c0c
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-11-12 22:43:56 +0100

    feature/chapter_8/examples/hashmaps: Added a examples of: Adding a Key and Value Only If a Key Isn’t Present, Updating a value based on the old value.

--- chapter_8/examples/hashmaps/src/main.rs
+++ chapter_8/examples/hashmaps/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let nazwa_drużyny_2 = String::from("Orzeł Klementowice");
     wyniki.insert(nazwa_drużyny_1, wynik_drużyny_1);
     wyniki.insert(nazwa_drużyny_2, wynik_drużyny_2);
-    // println!("HashMap 1: {:?}", X?);
+    println!("HashMap 1: {wyniki:?}");
 
     // Getting a value
     println!("Pobieranie pojedyńczej wartości.");
@@ -44,4 +44,32 @@ fn main() {
     wyniki.insert(String::from("Stella Lubomierz"), 25);
     let po_nadpisaniu_lubo = &wyniki.get(&wybrana_drużyna).copied().unwrap_or(0);
     println!("Wynik po nadpisaniu drużyny Stella Lubomierz: {po_nadpisaniu_lubo}");
+
+    // Adding a Key and Value Only If a Key Isn’t Present
+    println!("Dodawanie klucza z wartością, tylko przykadku klucz jeszcze nie istnieje.");
+    println!("Dodawanie Stelli Lubomierz (już istnieje)");
+    wyniki
+        .entry(String::from("Stella Lubomierz"))
+        .or_insert(100);
+    println!("Dodawanie „Orzeł Wojcieszów”, który jeszcze nie istnieje.");
+    //let dru_orzel = String::from("Orzeł Wojcieszów");
+    wyniki
+        .entry(String::from("Orzeł Wojcieszów"))
+        .or_insert(100);
+    println!(
+        "W „Stelli Lubomierz” nie powinna się zmienić punktacja ani nadpisać. Orzeł powinien być utworzony.\nWyniki: {wyniki:?}"
+    );
+
+    // Updating a value based on the old value
+    println!("Aktualizowanie wartośći opierając się natarej wartości");
+    let napis = "Witaj świecie przepiękny świecie";
+    let mut map = HashMap::new();
+
+    for słowo in napis.split_whitespace() {
+        // jeśli NIE znalazł słowa takiego, to zapisuje i przypisuje wartość 0
+        let licznik = map.entry(słowo).or_insert(0);
+        // czy znalazł, czy nie zawsze będzie +1. A więc gdy nie znajdzie, będzie 1, a gdy będzie+1
+        *licznik += 1;
+    }
+    println!("Ilość słów: {map:?}");
 }
