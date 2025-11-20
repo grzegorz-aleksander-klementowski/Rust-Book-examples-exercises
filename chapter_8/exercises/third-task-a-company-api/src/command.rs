8786bb1f2c32ed13ab3c88ee193f8cf40db15126
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-11-21 00:01:41 +0100

    feature: added and refactored handle incorrect commands test.

---
+++ chapter_8/exercises/third-task-a-company-api/src/command.rs
@@ -0,0 +1,232 @@
+use crate::*;
+
+#[derive(Debug, PartialEq)]
+pub enum Action {
+    Add,
+    List,
+}
+#[derive(Debug, PartialEq)]
+pub enum Object {
+    User(String),
+    People,
+}
+#[derive(Debug, PartialEq)]
+pub enum Operator {
+    To,
+    From,
+}
+#[derive(Debug, PartialEq)]
+pub enum Destination {
+    Department(String),
+    Company,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Command {
+    action: Option<Action>,
+    object: Option<Object>,
+    operator: Option<Operator>,
+    destination: Option<Destination>,
+}
+
+// Make possibili of creating custom extendable commands.
+// For example: "Add Procowój to Sales" will add the user to „Sales” departament, while
+// "List people from Sales" will list people from departament „Sales” but
+// "List people from Company" will list people from all of the departaments
+// Every needed keywors is typed while the rest return „None” exept the places when it's taing the
+// string like „user”, „departament”.
+impl Command {
+    pub fn parse_command(input: &str) -> Self {
+        let mut cmd = Command::default();
+
+        for (index, word) in input.split_whitespace().enumerate() {
+            match (index, word) {
+                (0, "add") => {
+                    cmd.action = Some(Action::Add);
+                }
+                (0, "list") => {
+                    cmd.action = Some(Action::List);
+                }
+                (0, _) => {
+                    cmd.action = None;
+                }
+                (1, "people") => {
+                    cmd.object = Some(Object::People);
+                }
+                (1, "") => {
+                    cmd.object = None;
+                }
+                (1, _) => {
+                    cmd.object = Some(Object::User(word.to_string()));
+                }
+                (2, "to") => {
+                    cmd.operator = Some(Operator::To);
+                }
+                (2, "from") => {
+                    cmd.operator = Some(Operator::From);
+                }
+                (2, _) => {
+                    cmd.operator = None;
+                }
+                (3, "company") => {
+                    cmd.destination = Some(Destination::Company);
+                }
+                (3, "") => {
+                    cmd.destination = None;
+                }
+                (3, _) => {
+                    cmd.destination = Some(Destination::Department(word.to_string()));
+                }
+                (_, _) => return Command::default(),
+            }
+        }
+        cmd
+    }
+
+    // It could reutrn a needed action, however, it return CommandHandler because of ability of testing
+    pub fn command_handler(&self, company: &mut HashMap<&str, Vec<&str>>) -> CommandResult {
+        match self {
+            Self {
+                action: Some(Action::Add),
+                object: Some(Object::User(user)),
+                operator: Some(Operator::To),
+                destination: Some(Destination::Department(department)),
+            } => add_a_user_to_a_departament(user, department, company),
+            Self {
+                action: Some(Action::List),
+                object: Some(Object::People),
+                operator: Some(Operator::From),
+                destination: Some(Destination::Department(department)),
+            } => list_ppl_in_a_department(company, department),
+            Self {
+                action: Some(Action::List),
+                object: Some(Object::People),
+                operator: Some(Operator::From),
+                destination: Some(Destination::Company),
+            } => list_people_in_the_company(company),
+            _ => CommandResult(Err(
+                "Incorrect command. Try again. \nHelp: use \"add `user` to `department_name`\" OR list people from `department_name`/`company`.".to_string(),
+            )),
+        }
+    }
+}
+
+impl Default for Command {
+    fn default() -> Self {
+        Self {
+            action: None,
+            object: None,
+            operator: None,
+            destination: None,
+        }
+    }
+}
+
+// Struct for better handling the results of the commands, making it also more testable
+#[derive(Debug, PartialEq, Eq)]
+pub struct CommandResult(pub Result<String, String>);
+
+// Printing the finall result of the commands.
+impl Display for CommandResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CommandResult(Ok(s)) => {
+                write!(f, "Wynik: {s}")
+            }
+            CommandResult(Err(e)) => {
+                write!(f, "Error: {e}")
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_parsing_command() {
+        // Test if the correct command works
+        let corr_add_user_cmd = Command {
+            action: Some(Action::Add),
+            object: Some(Object::User(String::from("Mądromira"))),
+            operator: Some(Operator::To),
+            destination: Some(Destination::Department("Sales".to_string())),
+        };
+        let user_input = "add Mądromira to Sales";
+        let result = Command::parse_command(user_input);
+        assert_eq!(result, corr_add_user_cmd);
+
+        let corr_list_ppl_dpt = Command {
+            action: Some(Action::List),
+            object: Some(Object::People),
+            operator: Some(Operator::From),
+            destination: Some(Destination::Department("Sales".to_string())),
+        };
+        let user_input = "list people from Sales";
+        let result = Command::parse_command(user_input);
+        assert_eq!(result, corr_list_ppl_dpt);
+
+        let corr_list_ppl_cmp = Command {
+            action: Some(Action::List),
+            object: Some(Object::People),
+            operator: Some(Operator::From),
+            destination: Some(Destination::Company),
+        };
+        let user_input = "list people from company";
+        let result = Command::parse_command(user_input);
+        assert_eq!(result, corr_list_ppl_cmp);
+
+        // Test if the command does't work
+        let bad_cases = [
+            // Test if the command with incorrect the command ending doesn't works
+            "Adder Mściwój to Sales",
+            // Test if the command with incorrect command does't work
+            "Gdd Strzeżymir to Sales",
+            // Test if mixed commadd doesn't works
+            "Sales Addes to Mściwój",
+            // Test if the departament name is incorrect it doesn't work
+            "Add Bolesław to Lublin",
+        ];
+
+        for case in bad_cases {
+            let user_command = case;
+            let result = Command::parse_command(user_command);
+            let expected = Command::default();
+            assert_eq!(result, expected);
+        }
+    }
+
+    #[test]
+    fn test_wrong_handling_commands() {
+        let mut company: HashMap<&str, Vec<&str>> = HashMap::new();
+
+        let incorr_list_usr = Command {
+            action: Some(Action::List),
+            object: Some(Object::User("Wrong".to_string())),
+            operator: Some(Operator::To),
+            destination: Some(Destination::Company),
+        };
+        let result = Command::command_handler(&incorr_list_usr, &mut company);
+        assert_eq!(result, CommandResult(Err("Incorrect command. Try again. \nHelp: use \"add `user` to `department_name`\" OR list people from `department_name`/`company`.".to_string())));
+
+        let incorr_add_ppl_cmp = Command {
+            action: Some(Action::Add),
+            object: Some(Object::People),
+            operator: Some(Operator::To),
+            destination: Some(Destination::Company),
+        };
+        let result = Command::command_handler(&incorr_add_ppl_cmp, &mut company);
+        assert_eq!(result, CommandResult(Err("Incorrect command. Try again. \nHelp: use \"add `user` to `department_name`\" OR list people from `department_name`/`company`.".to_string())));
+
+        let incorr_list_ppl_cmp = Command {
+            action: Some(Action::List),
+            object: Some(Object::People),
+            operator: Some(Operator::To),
+            destination: Some(Destination::Company),
+        };
+        let result = Command::command_handler(&incorr_list_ppl_cmp, &mut company);
+        assert_eq!(result, CommandResult(Err("Incorrect command. Try again. \nHelp: use \"add `user` to `department_name`\" OR list people from `department_name`/`company`.".to_string())));
+    }
+}
--- chapter_8/exercises/third-task-a-company-api/src/main.rs
+++ chapter_8/exercises/third-task-a-company-api/src/main.rs
@@ -1,3 +1,7 @@
+use command::*;
+
+mod command;
+
 // Using a hash map and vectors, create a text interface to
 // allow a user to add employee names to a department in a company;
 // for example, “Add Sally to Engineering” or “Add Amir to Sales.”
@@ -26,146 +30,6 @@ fn list_people_in_the_company(company: &HashMap<&str, Vec<&str>>) -> CommandResu
     todo!()
 }
 
-#[derive(Debug, PartialEq)]
-enum Action {
-    Add,
-    List,
-}
-#[derive(Debug, PartialEq)]
-enum Object {
-    User(String),
-    People,
-}
-#[derive(Debug, PartialEq)]
-enum Operator {
-    To,
-    From,
-}
-#[derive(Debug, PartialEq)]
-enum Destination {
-    Department(String),
-    Company,
-}
-
-#[derive(Debug, PartialEq)]
-struct Command {
-    action: Option<Action>,
-    object: Option<Object>,
-    operator: Option<Operator>,
-    destination: Option<Destination>,
-}
-
-// Make possibili of creating custom extendable commands.
-// For example: "Add Procowój to Sales" will add the user to „Sales” departament, while
-// "List people from Sales" will list people from departament „Sales” but
-// "List people from Company" will list people from all of the departaments
-// Every needed keywors is typed while the rest return „None” exept the places when it's taing the
-// string like „user”, „departament”.
-impl Command {
-    fn parse_command(input: &str) -> Self {
-        let mut cmd = Command::default();
-
-        for (index, word) in input.split_whitespace().enumerate() {
-            match (index, word) {
-                (0, "add") => {
-                    cmd.action = Some(Action::Add);
-                }
-                (0, "list") => {
-                    cmd.action = Some(Action::List);
-                }
-                (0, _) => {
-                    cmd.action = None;
-                }
-                (1, "people") => {
-                    cmd.object = Some(Object::People);
-                }
-                (1, "") => {
-                    cmd.object = None;
-                }
-                (1, _) => {
-                    cmd.object = Some(Object::User(word.to_string()));
-                }
-                (2, "to") => {
-                    cmd.operator = Some(Operator::To);
-                }
-                (2, "from") => {
-                    cmd.operator = Some(Operator::From);
-                }
-                (2, _) => {
-                    cmd.operator = None;
-                }
-                (3, "company") => {
-                    cmd.destination = Some(Destination::Company);
-                }
-                (3, "") => {
-                    cmd.destination = None;
-                }
-                (3, _) => {
-                    cmd.destination = Some(Destination::Department(word.to_string()));
-                }
-                (_, _) => return Command::default(),
-            }
-        }
-        cmd
-    }
-
-    // It could reutrn a needed action, however, it return CommandHandler because of ability of testing
-    fn command_handler(&self, company: &mut HashMap<&str, Vec<&str>>) -> CommandResult {
-        match self {
-            Self {
-                action: Some(Action::Add),
-                object: Some(Object::User(user)),
-                operator: Some(Operator::To),
-                destination: Some(Destination::Department(department)),
-            } => add_a_user_to_a_departament(user, department, company),
-            Self {
-                action: Some(Action::List),
-                object: Some(Object::People),
-                operator: Some(Operator::From),
-                destination: Some(Destination::Department(department)),
-            } => list_ppl_in_a_department(company, department),
-            Self {
-                action: Some(Action::List),
-                object: Some(Object::People),
-                operator: Some(Operator::From),
-                destination: Some(Destination::Company),
-            } => list_people_in_the_company(company),
-            _ => CommandResult(Err(
-                "Incorrect command. Try again. \nHelp: use \"Add `user` To `department_name`\" OR List people from `department_name`/`company`.".to_string(),
-            )),
-        }
-    }
-}
-
-impl Default for Command {
-    fn default() -> Self {
-        Self {
-            action: None,
-            object: None,
-            operator: None,
-            destination: None,
-        }
-    }
-}
-
-// Struct for better handling the results of the commands, making it also more testable
-#[derive(Debug, PartialEq, Eq)]
-struct CommandResult(Result<String, String>);
-
-// Printing the finall result of the commands.
-impl Display for CommandResult {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            CommandResult(Ok(s)) => {
-                write!(f, "Wynik: {s}")
-            }
-            CommandResult(Err(e)) => {
-                write!(f, "Error: {e}")
-            }
-        }
-    }
-}
-
 fn read_input() -> String {
     loop {
         let mut user_input = String::new();
@@ -189,8 +53,6 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-
-    use std::result;
 
     use super::*;
 
@@ -241,58 +103,5 @@ mod test {
             )),
             result
         )
-    }
-
-    #[test]
-    fn test_parsing_command() {
-        // Test if the correct command works
-        let corr_add_user_cmd = Command {
-            action: Some(Action::Add),
-            object: Some(Object::User(String::from("Mądromira"))),
-            operator: Some(Operator::To),
-            destination: Some(Destination::Department("Sales".to_string())),
-        };
-        let user_input = "add Mądromira to Sales";
-        let result = Command::parse_command(user_input);
-        assert_eq!(result, corr_add_user_cmd);
-
-        let corr_list_ppl_dpt = Command {
-            action: Some(Action::List),
-            object: Some(Object::People),
-            operator: Some(Operator::From),
-            destination: Some(Destination::Department("Sales".to_string())),
-        };
-        let user_input = "list people from Sales";
-        let result = Command::parse_command(user_input);
-        assert!(matches!(result, corr_list_ppl_dpt));
-
-        let corr_list_ppl_cmp = Command {
-            action: Some(Action::List),
-            object: Some(Object::People),
-            operator: Some(Operator::From),
-            destination: Some(Destination::Company),
-        };
-        let user_input = "list people from company";
-        let result = Command::parse_command(user_input);
-        assert!(matches!(result, corr_list_ppl_cmp));
-
-        // Test if the command does't work
-        let bad_cases = [
-            // Test if the command with incorrect the command ending doesn't works
-            "Adder Mściwój to Sales",
-            // Test if the command with incorrect command does't work
-            "Gdd Strzeżymir to Sales",
-            // Test if mixed commadd doesn't works
-            "Sales Addes to Mściwój",
-            // Test if the departament name is incorrect it doesn't work
-            "Add Bolesław to Lublin",
-        ];
-
-        for case in bad_cases {
-            let user_command = case;
-            let result = Command::parse_command(user_command);
-            let expected = Command::default();
-            assert!(matches!(result, expected));
-        }
     }
 }
