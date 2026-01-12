f368c6365e7edc111cba4376429dcc5d87b058df
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-01-12 12:59:38 +0100

    feat: Add the next step of the exmaple: added next list that took the largest number. The problem: the code is duplicated.

--- chapter_10/examples/largest-num/src/main.rs
+++ chapter_10/examples/largest-num/src/main.rs
@@ -15,4 +15,23 @@ fn main() {
             largest = num
         }
     }
+
+    println!("The largest number is: {largest}");
+
+    let my_number_list = vec![
+        821, 45, 176, 392, 67, 945, 112, 287, 31, 708, 154, 832, 349, 59, 690, 14, 573, 198, 461,
+        83, 917, 26, 305, 141, 624, 377, 108, 294, 859, 52, 681, 21, 458, 173, 902, 97, 336, 814,
+        63, 599, 417, 34, 742, 129, 268, 655, 74, 921, 185, 344, 519, 16, 788, 104, 273, 661, 92,
+        884, 58, 201,
+    ];
+
+    let mut largest = &my_number_list[0];
+
+    for num in &my_number_list {
+        if num > largest {
+            largest = num
+        }
+    }
+
+    println!("The largest number is: {largest}");
 }
