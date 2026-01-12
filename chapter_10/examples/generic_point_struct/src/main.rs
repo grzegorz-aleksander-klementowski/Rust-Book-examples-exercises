a78454a5cc6712bd721a3122f1730bdbf270e6ea
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-01-12 14:42:59 +0100

    feat: Added the example of struct Point that contains two type generics, to create stucts of integer, float and mix Point stucts.

---
+++ chapter_10/examples/generic_point_struct/src/main.rs
@@ -0,0 +1,12 @@
+// I did this example on my own
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main() {
+    let point_int = Point { x: 5, y: 10 };
+    let point_flt = Point { x: 5.0, y: 10.5 };
+    let point_mix = Point { x: 5, y: 10.5 };
+}
